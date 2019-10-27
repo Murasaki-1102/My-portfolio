e0bbbca43538c73c828d1a6233066ca7cd761b07
Murasaki-1102 <c011729114@edu.teu.ac.jp>
2019-10-27 17:17:32 +0900

    First commit

---
+++ js/profile.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar').addClass('nav-scroll');
+        } else {
+            $('.navbar').removeClass('nav-scroll');
+        }
+    });
+
+    $('a[href^="#"]').click(function () {
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+
+});
