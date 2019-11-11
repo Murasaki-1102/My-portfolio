f667e6284b0ec21f2d32e2a90bb695bdd3d43f25
Murasaki-1102 <c011729114@edu.teu.ac.jp>
2019-11-11 16:12:02 +0900

    slideanimation add

--- js/profile.js
+++ js/profile.js
@@ -18,5 +18,36 @@ $(function () {
     });
 
 
+    var slideContents = document.querySelectorAll('.slideContents');
+    var slideContentsRect = [];
+    var slideContentsTop = [];
+    var windowY = window.pageYOffset;
+    var windowH = window.innerHeight
+    var remainder = 100;
+
+    for (var i = 0; i < slideContents.length; i++) {
+        slideContentsRect.push(slideContents[i].getBoundingClientRect());
+    }
+
+    for (var i = 0; i < slideContentsRect.length; i++) {
+        slideContentsTop.push(slideContentsRect[i].top + windowY);
+    }
+
+    window.addEventListener('resize', function(){
+        windowH = window.innerHeight;
+    });
+
+    window.addEventListener('scroll', function(){
+        windowY = window.pageYOffset;
+
+        for (var i = 0; i < slideContents.length; i++) {
+            if(windowY > slideContentsTop[i] - windowH + remainder) {
+                slideContents[i].classList.add('show');
+            } else {
+                slideContents[i].classList.remove('show');
+            }
+        }
+    });
+
 
 });
